f46f8e6cb0e5bee449affbe2735794028dd76f2a
JustaDebianDistro <gabeks87609@yahoo.com>
2017-02-25 19:09:13 -0500

    redone windows version start
    now made with codeblocks

---
+++ Another-Dawn-Windows/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+    cout <<"Thanks for playing"<<endl
+    <<"We are sorry that this is all we have currently we try to update regularly "<<endl
+    <<"So make sure to check back soon"<<endl
+    << "Credits" << endl
+    <<"NetDelve"<<endl
+    <<"JustaDebianDistro && Nutcase84"<<endl
+    <<"Other Contributers"<<endl
+    <<" "<<endl;
+    system("Pause");
+    return 0;
+}
