625d41818a864154b5f74d499deb69b07131bf60
nutcase84 <cyruscarbone@gmail.com>
2017-03-20 12:59:44 -0400

    input.lua from laughing-engine

--- game.lua
+++ game.lua
@@ -6,5 +6,7 @@ function love.update()
 end
 
 function love.draw()
-  love.graphics.setBackgroundColor(100, 60, 100)
+	love.graphics.setBackgroundColor(100, 60, 100)
+	
+	love.graphics.rectangle("fill", player.x, player.y, 50, 50) --temporary "playermodel"
 end
