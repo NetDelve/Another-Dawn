eb644e7b86d320798e44aa49545aad1282e412c5
JustaDebianDistro <gabeks87609@yahoo.com>
2017-03-20 10:10:13 -0400

    fixed the printing
    took out name var

--- main.lua
+++ main.lua
@@ -2,5 +2,5 @@ require "game-art/artinclude"
 
 function love.draw()
   love.graphics.setBackgroundColor(100, 100, 100)
-	love.graphics.print("name: " .. name, 10, 30 )
+	love.graphics.print("name: " ,10, 30 )
 end
