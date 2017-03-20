20efd488cab422ebd8edbb856038a590b0680e8b
JustaDebianDistro <gabeks87609@yahoo.com>
2017-03-20 13:34:37 -0400

    movment fixed
    ...

--- game.lua
+++ game.lua
@@ -6,13 +6,13 @@ map = {} --objects to be rendered are put here
 mapAreas = {} --list of areas that will be put into the rendering queue
 
 function love.update(dt)
-	if love.keyboard.isDown("w") then --would probably be a good idea to add a "inputconf.lua" file at some point
+	if love.keyboard.isDown("a") then --would probably be a good idea to add a "inputconf.lua" file at some point
 		player.x = player.x - dt*player.moveSpeed
-	elseif love.keyboard.isDown("s") then
-		player.x = player.x + dt*player.moveSpeed
-	elseif love.keyboard.isDown("a") then
-		player.y = player.y - dt*player.moveSpeed
 	elseif love.keyboard.isDown("d") then
+		player.x = player.x + dt*player.moveSpeed
+	elseif love.keyboard.isDown("w") then
+		player.y = player.y - dt*player.moveSpeed
+	elseif love.keyboard.isDown("s") then
 		player.y = player.y + dt*player.moveSpeed
 	end
 end
