22ca754dd5a7025d2bd0c8da4c43bf8d65c07212
nutcase84 <cyruscarbone@gmail.com>
2017-03-21 14:25:42 -0400

    mapHandler.lua

---
+++ map/test/map.lua
@@ -0,0 +1,2 @@
+local area = {}
+table.insert(area, {x = 0, y = 0, sX = 50, sY = 50}) --test object
--- mapHandler.lua
+++ mapHandler.lua
@@ -1,11 +1,23 @@
 mapHandler = {}
 mapHandler.map = {} --objects to be rendered are put here, recalculated every time a new area is loaded
 mapHandler.globalIndex = require "map/globalIndex" --load array of all avalible areas
+mapHandler.loadedAreas = {}
+
+function mapHandler.loadAllSprites() --loads all images for all map areas, hopefully temporary until dynamic image loading is implimented
+	
+end
 
 function mapHandler.recalculateCheck(x, y, sX, sY) --checks to see if there is a area that could be drawn/seen but isn't loaded
-	
+	local drawableAreas = {}
+	for i,v in ipairs(globalIndex) do
+		if v.x >= x and (v.x+v.sX) <= x+sX and v.y >= y and (v.y+v.sY) <= x+sY then
+			table.insert(drawableAreas, v)
+		end
+	end
 end
 
 function mapHandler.recalculateAreas(x, y, sX, sY) --manages loaded areas based on player/camera position
 	
 end
+
+return mapHandler
