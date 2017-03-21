a8143f0011cd7a385c75313b5af37ff708428498
nutcase84 <cyruscarbone@gmail.com>
2017-03-21 13:35:41 -0400

    mapHandler.lua

--- mapHandler.lua
+++ mapHandler.lua
@@ -1,3 +1,7 @@
 mapHandler = {}
 map = {} --objects to be rendered are put here, recalculated every time a new area is loaded
 mapAreas = {} --list of areas that will be put into the rendering queue
+
+mapHandler.recalculateAreas(x, y, sX, sY) --manages loaded areas based on player/camera position
+	
+end
