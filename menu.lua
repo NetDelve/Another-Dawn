b38ceb9bbd54668452b0c311e7c89b59f32a10c4
JustaDebianDistro <gabeks87609@yahoo.com>
2017-03-20 21:11:59 -0400

    added start button
    ...

--- menu.lua
+++ menu.lua
@@ -1,6 +1,11 @@
 function love.draw()
   love.graphics.setBackgroundColor(100, 100, 100)
-	love.graphics.print("Another Dawn" , 10, 30 )
-	require "game"
+	love.graphics.print("Another Dawn v0.0.2" , 10, 30 )
+  love.graphics.print("Hit 'o' to play" , 10, 60 )
 end
 
+function love.update(dt)
+	if love.keyboard.isDown("o") then 
+		playgame()
+	end
+end
