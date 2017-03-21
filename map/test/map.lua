eb3b8c891df927503dfa8cd1191f56a953ff2ff1
nutcase84 <cyruscarbone@gmail.com>
2017-03-21 18:03:56 -0400

    mapHandler.lua

--- map/test/map.lua
+++ map/test/map.lua
@@ -1,2 +1,3 @@
 local area = {}
 table.insert(area, {x = 0, y = 0, sX = 50, sY = 50}) --test object
+return area
--- mapHandler.lua
+++ mapHandler.lua
@@ -7,16 +7,30 @@ function mapHandler.loadAllSprites() --loads all images for all map areas, hopef
 	
 end
 
+function mapHandler.loadArea(id) --loads area, but if called manually will get overwriten by recalculateAreas() if area isn't on screen
+	for i,v in ipairs(globalIndex) do
+		if v.id == id then
+			local insertTable = require(v.mapFile)
+			for i,v in ipairs(insertTable) do
+				table.insert(v, mapHandler.map)
+			end
+			return true
+			break
+		end
+	end
+	return false
+end
+
 function mapHandler.recalculateCheck(x, y, sX, sY) --checks to see if there is a area that could be drawn/seen but isn't loaded
 	drawableAreas = {}
 	for i,v in ipairs(mapHandler.globalIndex) do
 		if v.x >= x and (v.x+v.sX) <= x+sX and v.y >= y and (v.y+v.sY) <= x+sY then
-			table.insert(drawableAreas, v)
+			table.insert(drawableAreas, i)
 		end
 	end
 	for i,v in ipairs(drawableAreas) do
 		isInArray = false
-		for o,b in ipairs(mapHandler.globalIndex) do
+		for o,b in ipairs(mapHandler.loadedAreas) do
 			if i == o then
 				isInArray = true
 				break
@@ -35,10 +49,12 @@ function mapHandler.recalculateAreas(x, y, sX, sY) --manages loaded areas based 
 	mapHandler.loadedAreas = {}
 	for i,v in ipairs(mapHandler.globalIndex) do
 		if v.x >= x and (v.x+v.sX) <= x+sX and v.y >= y and (v.y+v.sY) <= x+sY then
-			table.insert(mapHandler.loadedAreas, v)
+			table.insert(mapHandler.loadedAreas, i)
 		end
 	end
 	for i,v in ipairs(mapHandler.loadedAreas) do
+		mapHandler.loadArea(mapHandler[v].id)
+	end
 end
 
 return mapHandler
