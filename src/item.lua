fb07b679caa17ea6d87cc821c0ea77a0c3508b39
JustaDebianDistro <gabeks87609@yahoo.com>
2018-01-02 16:20:19 -0500

    Added Item base

---
+++ src/item.lua
@@ -0,0 +1,11 @@
+item = {}
+
+item.stats = {}
+
+item.id = nil
+item.name = nil
+item.image = nil
+item.itemtype = nil
+item.stackable = false
+
+return item
--- src/player.lua
+++ src/player.lua
@@ -1,12 +1,28 @@
 player = {}
 
+player.inventory = {}
+player.questj = {}
+
+player.hp = 100
+player.ap = 5
+
+player.money = 100
+
+player.str = 4
+player.per = 4
+player.edr = 4
+player.cha = 4
+player.agl = 4
+player.int = 4
+player.luck = 4
+
 player.x = 0 --player location
 player.y = 0
 player.moveSpeed = 500
 
 player.sprite = nil --player sprite
 
-player.sX = 50 --player size
-player.sY = 50
+player.sX = 64 --player size
+player.sY = 64
 
 return player
