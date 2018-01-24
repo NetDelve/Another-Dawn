1ffcb6c70ec7dd1f2d98a2ec5de89bcf82e869a2
nutcase84 <nutcase84@protonmail.com>
2018-01-24 17:55:45 -0500

    stuff

--- src/bck/bck.lua
+++ src/bck/bck.lua
@@ -7,8 +7,8 @@ bck.world = {} --contains area tables that contain objects
 function bck.loadWorld(file)
 	bck.objects, bck.world = require(file)
 	for i,v in ipairs(bck.objects) do
-		if v.imageFile then
-			v.image = love.graphics.newImage(v.imageFile)
+		if v.imageFile ~= false and v.imageFile ~= nil then
+			v.image = love.graphics.newImage(v.imageFile) --load all object images
 		end
 	end
 end
@@ -17,7 +17,7 @@ function bck.update(dt) --TODO multithread object scripts
 	for i,area in ipairs(bck.world) do
 		for o,object in ipairs(area.foreground) do
 			if bck.objects[object.type].script ~= nil then
-				if not pcall(bck.objects[object.type].script(i,o) then --run object script with error protection, i and o are to tell the script what object to run on
+				if not pcall(bck.objects[object.type].script(i,o)) then --run object script with error protection, i and o are to tell the script what object to run on
 					--error
 				end
 			end
@@ -28,11 +28,11 @@ end
 function bck.drawForeground(camX, camY, sX, sY)
 	for i,area in ipairs(bck.world) do
 		for i,object in ipairs(area.foreground) do
-			if object.x+object.sX >= camX and object.x <= sX+camX and object.y+object.sY >= camY and object.y <= sY+camY then
+			if (object.x+area.x)+object.sX >= camX and (object.x+area.x) <= sX+camX and (object.y+area.y)+object.sY >= camY and (object.y+area.y) <= sY+camY then
 				if bck.objects[object.type].image ~= nil then
-					love.graphics.draw(bck.objects[object.type].image, object.x, object.y)
+					love.graphics.draw(bck.objects[object.type].image, object.x+area.x, object.y+area.y)
 				else
-					--missing image, draw some error thing
+					love.graphics.rectangle("fill", object.x+area.x, object.y+area.y, bck.objects[object.type].sX, bck.objects[object.type].sY)	--missing image, draw some error thing
 				end
 			end
 		end
@@ -42,11 +42,11 @@ end
 function bck.drawBackground(camX, camY, sX, sY)
 	for i,area in ipairs(bck.world) do
 		for i,object in ipairs(area.background) do
-			if object.x+object.sX >= camX and object.x <= sX+camX and object.y+object.sY >= camY and object.y <= sY+camY then
+			if (object.x+area.x)+object.sX >= camX and (object.x+area.x) <= sX+camX and (object.y+area.y)+object.sY >= camY and (object.y+area.y) <= sY+camY then
 				if bck.objects[object.type].image ~= nil then
-					love.graphics.draw(bck.objects[object.type].image, object.x, object.y)
+					love.graphics.draw(bck.objects[object.type].image, object.x+area.x, object.y+area.y)
 				else
-					--missing image, draw some error thing
+					love.graphics.rectangle("fill", object.x+area.x, object.y+area.y, bck.objects[object.type].sX, bck.objects[object.type].sY)	--missing image, draw some error thing
 				end
 			end
 		end
@@ -70,8 +70,16 @@ function bck.newObject(type, _sX, _sY, _solid, _imageFile, _script)
 	bck.objects[type] = {sX = _sX, sY = _sY, solid = _solid, imageFile = _imageFile, script = _script}
 end
 
-function bck.placeObject(area, _x, _y, _type)
-	bck.world[area] = {x = _x, y = _y, sX = _sX, sY = _sY, type = _type}
+function bck.newArea(id, _x, _y, _sX, _sY)
+	bck.world[id] = {x = _x, y = _y, sX = _sX, sY = _sY, foreground = {}, background = {}}
+end
+
+function bck.placeForeground(area, _x, _y, _type)
+	bck.world[area].foreground = {x = _x, y = _y, type = _type}
+end
+
+function bck.placeBackground(area, _x, _y, _type)
+	bck.world[area].background = {x = _x, y = _y, type = _type}
 end
 
 return bck
--- src/bck/editor.lua
+++ src/bck/editor.lua
@@ -1,14 +1,22 @@
+gui = require 'gui'
+
+config = {viewportSize = {x = 640, y = 480}}
+
+bck.newArea("test", 0, 0, 500, 500)
+bck.newObject("test", 50, 50, false, false)
+bck.placeForeground("test", 10, 10, "test")
+
 function love.update(dt)
 	if enableScripts then --TODO menu toggle
 		bck.update(dt)
 	end
-	frame = bck.drawToCanvas(camX, camY, config.viewportSize.y, config.viewportSize.y)
+	frame = bck.drawToCanvas(camX, camY, config.viewportSize.x, config.viewportSize.y)
 end
 
 function love.draw()
 	love.graphics.setColor(255,255,255,255)
 	love.graphics.setBlendMode("alpha", "premultiplied")
-	love.graphics.draw(frame, 0, 0, love.graphics.getWidth()-100, love.graphics.getHeight())
+	love.graphics.draw(frame, 0, 0, (love.graphics.getWidth()-100)/config.viewportSize.x, love.graphics.getHeight()/config.viewportSize.y)
 	love.graphics.setColor(100,100,100)
 	love.graphics.rectangle("fill", love.graphics.getWidth()-100, 0, 100, love.graphics.getHeight()) --right place object menu
 end
