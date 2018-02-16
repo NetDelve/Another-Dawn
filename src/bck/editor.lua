c3d663ff114609791636e1a82853399cd9dd1cc5
nutcase84 <nutcase84@protonmail.com>
2018-02-15 21:58:48 -0500

    suit

--- src/bck/editor.lua
+++ src/bck/editor.lua
@@ -1,8 +1,9 @@
 -- editor = {}
---gui = require 'gui'
+suit = require 'suit'
 
 config = {gridSize = {x = 50, y = 50}} --TODO make own file
 camX, camY = 0, 0
+camSpeed = 500
 
 bck.newArea("test", 0, 0, 50, 50)
 bck.newObject("test", 1, 1, false, false)
@@ -13,7 +14,29 @@ local rMenuCan = love.graphics.newCanvas()
 local lMenuCan = love.graphics.newCanvas()
 local popupMenuCan = love.graphics.newCanvas()
 
+local input = {text = ""}
 function love.update(dt)
+	suit.layout:reset(10, 10) --left menu (edit object)
+	suit.Input(input, suit.layout:row(80, 30))
+	
+	suit.layout:reset(love.graphics.getWidth()-90, 10) --right menu (place object)
+
+	suit.layout:reset(10, 10) --top menu (menu)
+
+	suit.layout:reset(10, love.graphics.getHeight()-10)
+
+	if not suit.anyActive() then
+		if love.keyboard.isDown("w") then
+			camY = camY - camSpeed*dt
+		elseif love.keyboard.isDown("s") then
+			camY = camY + camSpeed*dt
+		elseif love.keyboard.isDown("a") then
+			camX = camX + camSpeed*dt
+		elseif love.keyboard.isDown("d") then
+			camX = camX - camSpeed*dt
+		end
+	end
+
 	if enableScripts then --TODO menu toggle
 		bck.update(dt)
 	end
@@ -26,53 +49,16 @@ function love.draw()
 	love.graphics.draw(frame, 0, 0)
 	love.graphics.setBlendMode("alpha")
 
-	if type(editorState) == "string" then
-		love.graphics.setColor(125,125,125)
-		love.graphics.rectangle("fill", 0, 0, love.graphics.getWidth(), 50)
-		love.graphics.setColor(255,255,255,255)
-		love.graphics.print(editorState..": "..textbox, 10, 10)
-	end
-
-	--[[love.graphics.setColor(100,100,100)
+	--[[love.graphics.setColor(0,0,0)
 	love.graphics.rectangle("fill", love.graphics.getWidth()-100, 0, 100, love.graphics.getHeight())
-	love.graphics.rectangle("fill", 0, love.graphics.getHeight()-100, love.graphics.getWidth(), 100)
-	gui.draw()]]
+	love.graphics.rectangle("fill", 0, love.graphics.getHeight()-100, love.graphics.getWidth(), 100)]]
+	suit.draw()
 end
 
-textbox = ""
 function love.keypressed(key, scancode, isrepeat)
-	if not editorState then
-		if key == "o" then --new object
-			editorState = "newObject"
-		elseif key == "f" then --place object
-			editorState = "placeForeground"
-		elseif key == "b" then
-			editorState = "placeBackground"
-		elseif key == "a" then --new area
-			editorState = "newArea"
-		end
-	elseif key == "backspace" then
-		textbox = string.sub(textbox, 1, string.len(textbox)-1)
-	elseif key == "return" then
-		if editorState == "newObject" then
-			--bck.newObject(textbox, 
-		elseif editorState == "placeObject" then
-		
-		elseif editorState == "newArea" then
-		
-		end
-		editorState = false
-		textbox = ""
-	end
-	--gui.keypressed(key, isrepeat)
+	suit.keypressed(key)
 end
 
 function love.textinput(text)
-	if type(editorState) == "string" then
-		textbox = textbox..text
-	end
-end
-
-function love.mousepressed(x, y, button)
-	--gui.mousepressed(x, y, button)
+	suit.textinput(text)
 end
